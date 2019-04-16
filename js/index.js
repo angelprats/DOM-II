80948e80016e3e06addef5900f7dc86aadb3a4be
angel prats <angelpratz@gmail.com>
2019-04-16 16:47:32 -0400

    DOM II about half way

--- js/index.js
+++ js/index.js
@@ -2,5 +2,35 @@
 const funBus = document.querySelector('h1');
 funBus.addEventListener('mouseover', event => {
   event.target.style.color = 'blue';
-  console.log(funBus);
+  event.target.style.fontSize = '6rem';
+  // console.log(funBus);
 });
+
+const navigation = document.querySelector('.nav');
+navigation.addEventListener('dblclick', event => {
+  event.target.style.fontSize = '2.5rem';
+  event.target.style.color = 'crimson';
+});
+
+const secondP = document.querySelector('.text-content p');
+secondP.addEventListener('wheel', event => {
+  event.target.style.fontSize = '2.5rem';
+  event.target.style.color = 'crimson';
+  // console.log(secondP);
+});
+
+const introHead = document.querySelector('.intro p');
+introHead.addEventListener('click', event => {
+  event.target.style.color = 'blue';
+  event.target.style.fontFamily = 'Indie Flower';
+  event.target.style.fontSize = '2rem';
+  // console.log(introHead);
+});
+
+//mouseleave
+
+const boat = document.querySelector('.img-content');
+boat.addEventListener('mouseenter', event => {
+  event.target.style.height = '400 px';
+});
+console.log(boat);
