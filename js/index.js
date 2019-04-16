ca612e38d8557f460f102abb0866735e173baf22
angel prats <angelpratz@gmail.com>
2019-04-16 14:15:14 -0400

    dom II initial commit

--- js/index.js
+++ js/index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+const funBus = document.querySelector('h1');
+funBus.addEventListener('mouseover', event => {
+  event.target.style.color = 'blue';
+  console.log(funBus);
+});
